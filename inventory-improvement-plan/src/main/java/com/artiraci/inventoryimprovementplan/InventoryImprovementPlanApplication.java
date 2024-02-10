2faec5b696cb654434537ec1631c676017aa5312
Arturo Mendoza <arturocmi91@gmail.com>
2024-02-10 18:13:15 +0100

    started the project

---
+++ inventory-improvement-plan/src/main/java/com/artiraci/inventoryimprovementplan/InventoryImprovementPlanApplication.java
@@ -0,0 +1,13 @@
+package com.artiraci.inventoryimprovementplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryImprovementPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryImprovementPlanApplication.class, args);
+	}
+
+}
