c88fb18413908b0b9772f92adf6d4d8eb61c4f17
Arturo Mendoza <arturocmi91@gmail.com>
2024-03-16 18:49:34 +0100

    new impoves springDoc

--- inventory-improvement-plan/src/main/java/com/artiraci/inventoryimprovementplan/InventoryImprovementPlanApplication.java
+++ inventory-improvement-plan/src/main/java/com/artiraci/inventoryimprovementplan/InventoryImprovementPlanApplication.java
@@ -1,7 +1,11 @@
 package com.artiraci.inventoryimprovementplan;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryImprovementPlanApplication {
@@ -9,5 +13,16 @@ public class InventoryImprovementPlanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryImprovementPlanApplication.class, args);
 	}
+@Bean
+	public OpenAPI customOpenAPI(){
+		return  new OpenAPI()
+				.info(new Info()
+						.title("Inventory Project")
+						.version("0.11")
+						.description("Sample app with Swagger")
+						.termsOfService("http://swagger.io/terms/")
+						.license(new License().name("Apache2.0").url("https://springdoc.org")));
 
+
+}
 }
